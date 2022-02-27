6daa778232058feb8f2bdff45d330a291681fb28
Sir-Lord <60177652+Sirlord-Sen@users.noreply.github.com>
2022-02-27 10:49:16 -0600

    feat: register & login ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'authentication-app';
+  constructor(
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer
+  ){
+    this.iconRegistry.addSvgIcon(
+      'google',this.sanitizer.bypassSecurityTrustResourceUrl(
+        `/assets/icons/google.svg`
+      )
+      );
+    this.iconRegistry.addSvgIcon(
+      'facebook',this.sanitizer.bypassSecurityTrustResourceUrl(
+        `/assets/icons/facebook.svg`
+      )
+      );
+    this.iconRegistry.addSvgIcon(
+      'twitter',this.sanitizer.bypassSecurityTrustResourceUrl(
+        `/assets/icons/twitter.svg`
+      )
+      );
+    this.iconRegistry.addSvgIcon(
+      'github',this.sanitizer.bypassSecurityTrustResourceUrl(
+        `/assets/icons/github.svg`
+      )
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -19,6 +18,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { HttpClientModule } from '@angular/common/http';
     RegisterComponent,
     ProfileInfoComponent,
     EditProfileInfoComponent,
+    LogoComponent,
   ],
   imports: [
     HttpClientModule,
@@ -35,6 +37,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
+    MatButtonModule,
     MatToolbarModule,
     FlexLayoutModule,
     MatIconModule,
