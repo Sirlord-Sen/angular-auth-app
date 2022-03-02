a52f94c2510288f74366de3b9cfc94f51fa4b3b8
Sir-Lord <60177652+Sirlord-Sen@users.noreply.github.com>
2022-03-02 11:16:23 -0600

    deploy

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/authentication-app'));
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/authentication-app/index.html'))
+    res.sendFile(path.join(__dirname+ '/dist/authentication-app/index.html'))
 });
 app.listen(process.env.PORT || 8080);
