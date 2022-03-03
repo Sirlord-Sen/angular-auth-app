977814941cb2e63d7081113329fd70268c3e5bed
Sir-Lord <60177652+Sirlord-Sen@users.noreply.github.com>
2022-03-02 20:26:12 -0600

    deploy

--- server.js
+++ server.js
@@ -1,35 +1,16 @@
-const router = require('koa-router')()
-const body = require('koa-body')({ text: false });
-const send = require('koa-send');
-const fs = require('fs');
-
-async function main(ctx, next) {
-    //All dynamic routes start with "/api"
-    if (/\/api\//.test(ctx.path)) {
-        try {
-            await next();
-        }
-        catch (error) {
-            if (error instanceof ApplicationError) {
-                console.log(error, { data: error.data, stack: error.stack });
-                ctx.status = error.code;
-            } else {
-                ctx.status = 500;
-                console.log(error.message, { stack: error.stack });
-            }
-        }
-        return;
-    } else {
-        //Not a dynamic route, serve static content
-        if ((ctx.path != "/") && (fs.existsSync('dist' + ctx.path))) {
-            await send(ctx, 'dist' + ctx.path);
-        } else {
-            await send(ctx, 'dist/index.html');
-        }
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
     }
+    next();
 }
+const express = require('express');
+const app = express();
+app.use(requireHTTPS);
+app.use(express.static('./dist/authentication-app'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/authentication-app/'});
+});
 
-module.exports = app => {
-    app.use(main);
-    app.use(router.routes());
-};
+app.listen(process.env.PORT || 8080)
