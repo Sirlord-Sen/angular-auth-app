670061c0244687004886c6304563bd8caab68e48
Sir-Lord <60177652+Sirlord-Sen@users.noreply.github.com>
2022-03-02 20:08:02 -0600

    deploy

--- server.js
+++ server.js
@@ -1,43 +1,35 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// app.use(express.static(__dirname + '/dist/authentication-app'));
-// app.get('/*', function(req,res) {
-//     res.sendFile(path.join(__dirname+ '/dist/authentication-app/index.html'))
-// });
-// app.listen(process.env.PORT || 8080);
+const router = require('koa-router')()
+const body = require('koa-body')({ text: false });
+const send = require('koa-send');
+const fs = require('fs');
 
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const app = express();
+async function main(ctx, next) {
+    //All dynamic routes start with "/api"
+    if (/\/api\//.test(ctx.path)) {
+        try {
+            await next();
+        }
+        catch (error) {
+            if (error instanceof ApplicationError) {
+                console.log(error, { data: error.data, stack: error.stack });
+                ctx.status = error.code;
+            } else {
+                ctx.status = 500;
+                console.log(error.message, { stack: error.stack });
+            }
+        }
+        return;
+    } else {
+        //Not a dynamic route, serve static content
+        if ((ctx.path != "/") && (fs.existsSync('dist' + ctx.path))) {
+            await send(ctx, 'dist' + ctx.path);
+        } else {
+            await send(ctx, 'dist/index.html');
+        }
+    }
+}
 
-const Webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-/** this is custom js to help proxy in server.js*/
-const webpackConfig = require('./webpack.config.js');
-
-
-const compiler = Webpack(webpackConfig);
-const devServerOptions = { ...webpackConfig.devServer, open: true };
-const server = new WebpackDevServer(devServerOptions, compiler);
-
-const runServer = async () => {
-  console.log('Starting server...');
-  await server.start();
+module.exports = app => {
+    app.use(main);
+    app.use(router.routes());
 };
-
-runServer();
-/** If you have error creating proxy <app-url> to localhost  
- * Heroku internally redirect the Server port 8080 .
- *  For that reason we need to open listener  port(I used 3000 here) redirect 
-through http-proxy-middleware*/
- app.use("/*", createProxyMiddleware(
-    { target: "https://localhost:8080", 
-    ws: true ,
-     changeOrigin: true,
-     secure:false,
-     router: {
-    'dev.localhost:3000': 'https://localhost:8080',
-  },})) 
-
-app.listen(process.env.PORT || 3000)
